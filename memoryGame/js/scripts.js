9963fba6da34cc3ee6b513f3bf48b3cf9b556284
Alex Johnson <iamalexjohnson7@gmail.com>
2020-06-21 14:47:16 -0600

    started JavaScript

--- memoryGame/js/scripts.js
+++ memoryGame/js/scripts.js
@@ -1,1 +1,31 @@
-alert('working');
+
+// setup game board
+//when item is clicked it flips over
+// if two items are flipped check if they are the same
+// check that same card isnt clicked twice.
+const gameBoard = document.getElementById('game-board');
+const cardArr = [1,1,2,2,3,3,4,4];
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+
+function setupGame(){
+    shuffleArray(cardArr);
+    setupCards();
+    
+}
+
+function setupCards() {
+    cardArr.forEach((a) => {return gameBoard.innerHTML += `<div id='card'>${a}</div>`});
+}
+
+
+setupGame();
+
+
+
