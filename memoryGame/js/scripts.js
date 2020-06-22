0daf9a924f386d875e42e373a70389bb20ba9b04
Alex Johnson <iamalexjohnson7@gmail.com>
2020-06-22 17:04:08 -0600

    worked on javascript, added sass

--- memoryGame/js/scripts.js
+++ memoryGame/js/scripts.js
@@ -5,6 +5,12 @@
 // check that same card isnt clicked twice.
 const gameBoard = document.getElementById('game-board');
 const cardArr = [1,1,2,2,3,3,4,4];
+let clickedArr = [];
+let itemArr=[];
+let itemClass =[];
+let cardMatches = 0;
+const infoBox = document.getElementById('info-box');
+let attempts = 0;
 
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -15,17 +21,86 @@ function shuffleArray(array) {
 
 
 function setupGame(){
+    gameBoard.innerHTML = "";
     shuffleArray(cardArr);
     setupCards();
-    
+    infoBox.innerHTML ="";
 }
 
 function setupCards() {
-    cardArr.forEach((a) => {return gameBoard.innerHTML += `<div id='card'>${a}</div>`});
+    cardArr.forEach((a,b) => {return gameBoard.innerHTML += `<div id='card' class='card${b}'>${a}</div>`});
+    
+}
+
+function resetClicks (){
+    clickedArr = [];
+    itemArr = [];
+    itemClass = [];
+}
+function removeClass() {
+    itemArr.map(a => {a.classList.remove('active')});
+    resetClicks();
+}
+
+function checkClick (){
+    let classList = event.srcElement.className.split(/\s+/);
+       
+        // console.log(`CLASS LIST: ${classList}`);
+        // itemClass.forEach(a => {console.log(a)});
+
+        // console.log(`ITEM: ${itemClass[0]} CLASS: ${classList[0]}`);
+        // console.log(`BOOLEAN: ${itemClass[0] !== classList[0]}`);
+ 
+    
+            if(itemArr.length <= 1 && itemClass[0] !== classList[0]){
+                
+                itemClass.push(event.target.classList[0]);
+                itemArr.push(event.target);
+    
+                if(clickedArr.length  <= 2 ){
+                    clickedArr.push(event.target.innerHTML);
+                    event.target.classList.add('active');
+                }
+            }
+
+
+
+    if(clickedArr.length === 2){
+        if(clickedArr[0] === clickedArr[1]){
+            setTimeout(function () { itemArr.map(a => {a.classList.remove('active'); a.classList.add('matched');}) }, 300); 
+            // console.log('matched');
+            cardMatches++;
+        }
+        attempts++;
+        setTimeout(function () { removeClass() }, 500);
+        
+    }
+
+        
+
+
+
+
+    if(cardMatches === 4){
+        setTimeout(function () { infoBox.innerHTML = `<p>You Win! Attempts: ${attempts}</p> <button id='reset' onClick='setupGame()'>Reset</button>`; }, 500);
+        // console.log('win');
+    }
+    // console.log(`MATCHES: ${cardMatches}`);
+
+    // console.log(`ITEM ARR: ${itemArr}`);
+
+    // console.log(`CARD CLASSES: ${cardClass}`);
+    // console.log(`EVENT: ${event} TARGET: ${event.target}`);
+    // console.log(`CLICKED ARR: ${clickedArr}`);
 }
 
 
 setupGame();
 
 
+document.addEventListener('click', function (event) {
+	// If the clicked element doesn't have the right selector, bail
+    if (!event.target.matches('#card')) return;
+    checkClick();   
 
+}, false);
