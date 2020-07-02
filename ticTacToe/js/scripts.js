8eabd0851b4ddb74891271be2f9e7f0c371c9050
Alex Johnson <iamalexjohnson7@gmail.com>
2020-07-02 16:04:17 -0600

    updated js

--- ticTacToe/js/scripts.js
+++ ticTacToe/js/scripts.js
@@ -6,6 +6,39 @@ let gameArr = ["","","","","","","","",""];
 //     itemBox[i].style.backgroundColor = "red";
 // }
 
+function userTurn(current){
+    if(gameArr[current] === ""){
+        gameArr[current] = "X";
+        addElements();
+    }
+}
+
+function computerTurn(num) {
+    console.log(`GAME ARR: ${gameArr}`);
+    while(gameArr[num] !== ""){
+        console.log("nope");
+        num = randomNum();
+    }
+    if(gameArr[num] == ""){
+        console.log("ye");
+        gameArr[num] = "O";
+        addElements();
+    } 
+
+}
+
+function randomNum(){
+    console.log(Math.floor(Math.random() * gameArr.length));
+    return Math.floor(Math.random() * gameArr.length);
+}
+
+
+function addElements(){
+    for(let i = 0; i < gameArr.length; i++){
+        document.getElementById(i).innerHTML = gameArr[i];
+    }
+}
+
 document.addEventListener('click', function (event) {
 
 	// If the clicked element doesn't have the right selector, bail
@@ -15,13 +48,11 @@ document.addEventListener('click', function (event) {
 	event.preventDefault();
 
 	// Log the clicked element in the console
-    console.log(event.target.id);
+    // console.log(event.target.id);
     let current = event.target.id;
 
-    if(gameArr[current] === ""){
-        gameArr[current] = "X";
-        event.target.innerHTML = "X";
-    }
+    userTurn(current);
+    computerTurn(randomNum(), current);
 
     console.log(`GAME ARR: ${gameArr}`);
 }, false);
